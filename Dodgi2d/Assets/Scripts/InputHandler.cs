bbde10343e78519fe3e3e1219bf67bf5e296b487
NickAk <asoman97@gmail.com>
2018-03-17 16:17:51 +0300

    Initial commit with project

---
+++ Dodgi2d/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,85 @@
+﻿using Lean.Touch;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+
+    LeanFinger mainFinger;
+
+    void OnEnable()
+    {
+        LeanTouch.OnFingerDown += OnFingerDown;
+        LeanTouch.OnFingerUp += OnFingerUp;
+        LeanTouch.OnFingerSet += OnFingerSet;
+    }
+
+    void OnDisable()
+    {
+        LeanTouch.OnFingerDown -= OnFingerDown;
+        LeanTouch.OnFingerUp -= OnFingerUp;
+        LeanTouch.OnFingerSet -= OnFingerSet;
+    }
+
+
+    
+
+    private void OnFingerDown(LeanFinger finger)
+    {
+        if(mainFinger == null)
+        {
+            mainFinger = finger;
+            SetDirectionAndRotation(finger);
+        }
+    }
+
+    private void OnFingerUp(LeanFinger finger)
+    {
+        if (finger == mainFinger)
+        {
+            mainFinger = null;
+            SetDirection(Vector3.zero);
+        }
+    }
+
+
+    private void OnFingerSet(LeanFinger finger)
+    {
+        if (finger == mainFinger)
+        {
+            SetDirectionAndRotation(finger);
+        }
+    }
+
+    private void SetDirectionAndRotation(LeanFinger finger)
+    {
+        Vector3 worldFingerPosition = GetFingerWorldPosition(finger);
+        SetDirection(FingerPositionToDirection(worldFingerPosition));
+        SetRotation(worldFingerPosition);
+    }
+
+    private void SetDirection(Vector3 direction)
+    {
+        GetComponent<PlayerMove>().SetDirection(direction);
+    }
+
+    private void SetRotation(Vector3 point)
+    {
+        GetComponent<PlayerMove>().SetRotation(point);
+    }
+
+    private Vector3 GetFingerWorldPosition(LeanFinger finger)
+    {
+        return finger.GetWorldPosition(0, Camera.main);
+    }
+
+    private Vector3 FingerPositionToDirection(Vector3 position)
+    {
+        Vector3 direction =  position - transform.position;
+        direction.z = 0;
+        return direction;
+
+    }
+
+    
+}
+
---
+++ Dodgi2d/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float speed = 10.0f;
+
+    Vector3 direction = new Vector3();
+    GameObject sprite;
+	
+    
+    void Start()
+    {
+        sprite = transform.Find("Sprite").gameObject;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (direction.magnitude > 0.1)
+        {
+            direction = Vector3.ClampMagnitude(direction, 3);
+            Debug.Log(direction.magnitude);
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+	}
+
+    public void SetDirection(Vector3 direction)
+    {
+        Debug.Log(direction);
+        this.direction = direction;
+    }
+
+    public void SetRotation(Vector3 point)
+    {
+        float AngleRad = Mathf.Atan2(point.x - transform.position.x, point.y - transform.position.y);
+        // Get Angle in Degrees
+        float AngleDeg = (180 / Mathf.PI) * AngleRad;
+        // Rotate Object
+        sprite.transform.rotation = Quaternion.Euler(0, 0, -AngleDeg);
+    }
+}
